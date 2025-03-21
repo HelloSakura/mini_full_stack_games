6f3b5d7f1e646a0f21542627c12516c539e2cd33
lucida <Lucida@aladinfun.com>
2025-03-21 12:45:28 +0800

    add server common

---
+++ apps/server/src/Common/Common.ts
@@ -0,0 +1,9 @@
+/**
+* @author Lucida
+* @description: 全局共用的一些常量
+* @date: 2025/02/06
+*/
+
+export enum StateTypeEnum{
+    Init = 0
+}
---
+++ apps/server/src/Common/Enum.ts
@@ -0,0 +1,23 @@
+/**
+* @author Lucida
+* @description: 枚举定义
+* @date: 2025/02/21
+*/
+
+
+export enum InputTypeEnum {
+    ActorMove = 'ActorMove',    //角色移动
+    WeaponShoot = 'WeaponShoot',    //开火
+    TimePast = 'TimePast',  //时间流逝
+}
+
+
+export enum EntityTypeEnum{
+    Init = 'Init',
+    Actor1 = 'Actor1',
+    Map = 'Map',
+    Weapon1 = 'Weapon1',
+    Bullet1 = 'Bullet1',
+    Bullet2 = 'Bullet2',
+    Explosion = 'Explosion',
+}
